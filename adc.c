4f2d59b95390896eff57ce4e42025dd859639faf
Dominik Dettling <s190466@student.dhbw-mannheim.de>
2022-05-03 09:32:42 +0200

    Bit order test
    Tested if the order of the bits of the arrays are correct or if there is a mistake.

--- adc.c
+++ adc.c
@@ -7,17 +7,10 @@
 void initTIM2(void); 
 void TIM2_IRQHandler(void);
 void initADC1(void); 
-void initUSART2(void); 
-void sendUSART2(int8_t number);
 
-volatile uint16_t result = 0; 
 volatile uint8_t adc1[LENGTH]; 
-volatile uint8_t adc2[LENGTH];
-volatile int16_t value1, value2, value3; 
-/* Array for the difference between value adc1 and adc2 */
-volatile int8_t adc3[LENGTH]; 
-/*Increment variables*/
-volatile int i1, i2, i3, n, z;
+volatile int16_t adcValue1; 
+int i1, n; 
 
 int main(void)
 {
@@ -25,65 +18,31 @@ int main(void)
 	/*Init functions*/
 	initADC1();
 	initTIM2(); 
-	initUSART2(); 
 	
 	/*main loop*/
 	while(1)
 	{	
-			if((i1 == LENGTH) && (i2 == LENGTH))
+		
+			ADC1->CR2 |= (1<<30);		//Starts the adc conversion 
+			while(!(ADC1->SR & 2));
+			adc1[i1++]= (ADC1->DR >> 8);			//The highest 4 bits of the 12 bit adc value are saved in the array
+			adc1[i1++]=  ADC1->DR;
+			adcValue1 = (adc1[n] | (adc1[n+1]));
+			n++; 
+			if((n == (LENGTH-1)) || (i1 == (LENGTH-1)))
 			{
-				for(n = 0; n < (LENGTH - 1); n++)
-				{
-					sendUSART2(adc1[n]);
-					sendUSART2(adc1[n+1]);
-					sendUSART2(adc2[n]);
-					sendUSART2(adc2[n+1]);
-					adc3[n] = adc2[n] - adc1[n];
-					adc3[n+1] = adc2[n+1] - adc1[n+1];
-					sendUSART2(adc3[n]);
-					sendUSART2(adc3[n+1]);
-					/* In order to skip a byte an extra increment is made at the end of the for loop */
-					n++;
-				}
-				i1 = 0;
-				i2 = 0; 
-				i3 = 0;
+				n = 0; 
+				i1 = 0; 
 				memset(adc1, 0, sizeof(adc1));
-				memset(adc2, 0, sizeof(adc2)); 
-				memset(adc3, 0, sizeof(adc3));
 			}
+		
 	}
 }
 
 void TIM2_IRQHandler(void){
 
 	TIM2->SR = 0; 													//Clear interrupt flag
-	GPIOA->ODR ^= (1<<0);										//toggle PA0
-	if((i1 < LENGTH) || (i2 < LENGTH))
-	{
-//		if(ADC1->DR == 0x00 && (i1 == i2))
-//		{
-//			z++;
-//		}
-		if(GPIOA->ODR && (0x01))   					 //Rising edge 
-		{
-			/* Low values */
-			adc1[i1++]= (ADC1->DR >> 8);			//The highest 4 bits of the 12 bit adc value are saved in the array
-			adc1[i1++]=  ADC1->DR;
-//			value1 = ADC1->DR; 
-		}
-		else
-		{
-			/* High values */
-			adc2[i2++] = (ADC1->DR >> 8); 
-			adc2[i2++]=   ADC1->DR;
-//			value2 = ADC1->DR; 
-//			value3 = value2 - value1; 
-//			adc3[i3++] = (value3 >> 8);
-//			adc3[i3++] = value3; 
-		}
-	}
-	ADC1->CR2 |= (1<<30);								//Starts the adc conversion 
+	GPIOA->ODR ^= (1<<0);										//toggle PA0 
 }
 
 
@@ -125,37 +84,5 @@ void initADC1(void){
 	ADC1->SMPR2 |= (111 < 3); 		//480 cycles sampling time, Reference Manual 13.13.5, Berechnung 2. Tag 
 	ADC1->CR2 = 0;								//clear
 	ADC1->CR2 = (1<<0); //|| (1<<11); 					//Enable ADC1 and align the data to the left side for an easier signed value calculation
-	
-}
-
-void initUSART2(void){
- 
-	/*Configure Clock*/
-	RCC->AHB1ENR |= 1; 
-	RCC->APB1ENR |= (1 << 17);
-	
-	/*Configure PA2 for USART2_TX */
-	GPIOA->AFR[0] |= (7 << 8);
-	GPIOA->AFR[0] |= (7 << 12);
-	GPIOA->MODER |= (2 << 4);
-	GPIOA->MODER |= (2 << 6);
-	
-	/*Configure UART*/
-	USART2->BRR |= (104 << 4) | (3 << 0);   //Mantissa and Fraction
-	USART2->CR1 = 0x00; 									 	//Reset all
-	USART2->CR1 &= ~(1<<12); 								//Word length 
-	USART2->CR1 |= (1<<3);									//Enable TE
-	USART2->CR2 = 0x00;											//1 Stop Bit
-	USART2->CR3 = 0x00;											//No flow control 
-	USART2->CR1 |= (1<<13);									//Enable USART2
-	
-}
-
-void sendUSART2(int8_t number){
-
-	volatile char cache = 0;
-	cache = (char)number;									//Typumwandlung 
-	while(!(USART2->SR & (1<<7)));
-	USART2->DR |= cache; 
 	
 }
