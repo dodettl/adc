98f3c2520d1cbc236e3c6e460194026b509d68f3
Dominik Dettling <s190466@student.dhbw-mannheim.de>
2022-05-12 12:38:30 +0200

    Outsourced functions

--- Peripherie.c
+++ Peripherie.c
@@ -146,3 +146,114 @@ void CAN2_TX_IRQHandler (void)
 	  CAN_TxRdy[1] = 1; 
   }
 }
+
+void initUSART2(void){
+ 
+	/*Configure Clock*/
+	RCC->AHB1ENR |= 1; 
+	RCC->APB1ENR |= (1 << 17);
+	
+	/*Configure PA2 for USART2_TX */
+	GPIOA->AFR[0] |= (7 << 8);
+	GPIOA->AFR[0] |= (7 << 12);
+	GPIOA->MODER |= (2 << 4);
+	GPIOA->MODER |= (2 << 6);
+	
+	/*Configure UART*/
+	USART2->BRR |= (104 << 4) | (3 << 0);   //Mantissa and Fraction
+	USART2->CR1 = 0x00; 									 	//Reset all
+	USART2->CR1 &= ~(1<<12); 								//Word length 
+	USART2->CR1 |= (1<<3);									//Enable TE
+	USART2->CR2 = 0x00;											//1 Stop Bit
+	USART2->CR3 = 0x00;											//No flow control 
+	USART2->CR1 |= (1<<13);									//Enable USART2
+	
+}
+
+void sendUSART2(int8_t number){
+
+	volatile char cache = 0;
+	cache = (char)number;									//Typumwandlung 
+	while(!(USART2->SR & (1<<7)));
+	USART2->DR |= cache; 
+	
+}
+
+int16_t digitalIIRFilter(int16_t inputValue)
+{
+	static int32_t _2nw; 
+	int32_t _2ny; 
+	
+	_2ny = inputValue + _2nw;
+	_2nw = inputValue + _2ny - (_2ny>>k);
+	
+	return (_2ny>>(k+1)); 
+}
+
+int16_t digitalFIRFilter(int16_t inputValue)
+{
+	volatile static int16_t storeValues[10]; 
+	volatile int16_t filteredValue = 0; 
+	volatile static uint8_t counter; 
+	
+	if(counter < 10)
+	{
+		storeValues[counter] = inputValue; 
+		counter++; 
+	}
+	else
+	{
+		for(counter = 0;counter < 10; counter++)
+		{
+			filteredValue += storeValues[counter];
+		}
+		counter = 0; 
+		memset(storeValues, 0, sizeof(storeValues)); 
+		return(filteredValue/10); 
+	}
+	
+	return 0; 
+}
+
+
+
+void initTIM2(void){
+
+	/*Disable interrupt*/
+	__disable_irq(); 						
+	
+	/*Init GPIOA Port*/
+	RCC->AHB1ENR |= 1; 					
+	GPIOA->MODER &= ~(3<<0);		//Clear Pin PA0 
+	GPIOA->MODER |= (1<<0);			//Set Pin PA0 as an output 
+
+	/*Init TIM2*/
+	RCC->APB1ENR |= 1; 
+	TIM2->PSC = 16 - 1; 	//16 MHz divided by 16 => 1Mhz
+	TIM2->ARR = 100 - 1; 	//Counter 100 => 1Mhz/100 = 10 kHz
+	TIM2->CR1 = 1; 
+
+	/*Setup interrupt*/
+	TIM2->DIER |= 1; 
+	NVIC_EnableIRQ(TIM2_IRQn);
+	
+	/*Enable interrupt*/
+	__enable_irq();
+
+}
+
+void initADC1(void){
+
+	/*Configure clock*/
+	RCC->AHB1ENR |= 1;						//Enable GPIOA clock 
+	GPIOA->MODER |= (3<<2); 			//Analog mode PA1
+	
+	/*Configure ADC1*/
+	RCC->APB2ENR |= (1<<8);				//Enable ADC1 clock 
+	ADC1->SQR3 = 1;								//Conversion sequence starts at channel 1
+	ADC1->SQR1 = 0; 						  //Conversion length 1
+	ADC1->SMPR2 |= (111 < 3); 		//480 cycles sampling time, Reference Manual 13.13.5, Berechnung 2. Tag 
+	ADC1->CR2 = 0;								//clear
+	ADC1->CR2 = (1<<0); //|| (1<<11); 					//Enable ADC1 and align the data to the left side for an easier signed value calculation
+	
+}
--- Peripherie.h
+++ Peripherie.h
@@ -1,13 +1,39 @@
+/**
+  ******************************************************************************
+  * @file           : peripherie.h
+	* @author					: Dettling, Dominik 
+  * @brief          : Header for peripherie.c file.
+  *                   Header file of the used peripherals for the lvdt sensor
+  ******************************************************************************
+  */
 #ifndef __PERIPHERIE_H
 #define __PERIPHERIE_H
 
+/* Includes ------------------------------------------------------------------*/
 #include "stdint.h"
 
-#define STANDARD_FORMAT  0
+/* Defines -------------------------------------------------------------------*/
 
+/**
+  * @brief  defines for CAN
+  */
+#define STANDARD_FORMAT  0
 #define DATA_FRAME       0
 
+/**
+  * @brief  defines for digital IIR filter
+  */
+#define k 4
 
+/**
+  * @brief  define to enable USART2 in main.c
+  */
+#define ENABLE_DEBUGGING_INTERFACE 0
+
+/* Typedefs -----------------------------------------------------------------*/
+/**
+  * @brief  init structure CAN message
+  */
 typedef struct  {
   unsigned int   id;                    /* 29 bit identifier */
   unsigned char  data[8];               /* Data field */
@@ -16,7 +42,14 @@ typedef struct  {
   unsigned char  type;                  /* 0 - DATA FRAME, 1 - REMOTE FRAME */
 } CAN_msg;
 
-/* Functions defined in module CAN.c */
+/* Gobal Variables ----------------------------------------------------------*/
+extern CAN_msg CAN_TxMsg[2];      			/* CAN messge for sending */                              
+extern unsigned int CAN_TxRdy[2];      /* CAN HW ready to transmit a message */
+
+/* Function prototypes -------------------------------------------------------*/
+/**
+  * @brief  function prototypes for CAN2
+  */
 void initCAN2(void); 
 void setupCAN2(void);
 void startCAN2(void);
@@ -24,9 +57,16 @@ void waitReadyCAN2(void);
 void initMsgCAN2(void); 
 void wrMsgCAN2(CAN_msg *msg);
 
-extern CAN_msg       CAN_TxMsg[2];      /* CAN messge for sending */
-extern CAN_msg       CAN_RxMsg[2];      /* CAN message for receiving */                                
-extern unsigned int  CAN_TxRdy[2];      /* CAN HW ready to transmit a message */
-extern unsigned int  CAN_RxRdy[2];      /* CAN HW received a message */
+void Delay(uint32_t dlyTicks);
+void SysTick_Handler(void);
+
+void initTIM2(void); 
+void TIM2_IRQHandler(void);
+void initADC1(void); 
+void initUSART2(void); 
+void sendUSART2(int8_t number);
+int16_t digitalIIRFilter(int16_t inputValue); 
+int16_t digitalFIRFilter(int16_t inputValue); 
+
 
 #endif
